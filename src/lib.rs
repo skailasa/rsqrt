898e62201a6079951c720623d26a868b837b6eed
Srinath Kailasa <srinathkailasa@gmail.com>
2024-05-21 09:08:32 +0100

    Lint

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,31 @@
-use std::arch::x86_64::{__m128, __m128d, __m256, __m256d, _mm256_andnot_ps, _mm256_cmp_ps, _mm256_cvtpd_ps, _mm256_cvtps_pd, _mm256_mul_pd, _mm256_mul_ps, _mm256_rsqrt_ps, _mm256_set1_pd, _mm256_set1_ps, _mm256_set_ps, _mm256_sub_pd, _mm256_sub_ps, _mm_andnot_ps, _mm_cmpeq_ps, _mm_rsqrt_ps, _CMP_EQ_OS};
 use pulp::{f32x4, f32x8};
-
+use std::arch::x86_64::{
+    __m128, __m128d, __m256, __m256d, _mm256_andnot_ps, _mm256_cmp_ps, _mm256_cvtpd_ps,
+    _mm256_cvtps_pd, _mm256_mul_pd, _mm256_mul_ps, _mm256_rsqrt_ps, _mm256_set1_pd, _mm256_set1_ps,
+    _mm256_set_ps, _mm256_sub_pd, _mm256_sub_ps, _mm_andnot_ps, _mm_cmpeq_ps, _mm_rsqrt_ps,
+    _CMP_EQ_OS,
+};
 
 /// Single precision rsqrt
 pub fn rqsqrt_approx_intrin_avx_32(r2: __m256) -> __m256 {
     let zero = f32x8(0., 0., 0., 0., 0., 0., 0., 0.);
     unsafe {
-        _mm256_andnot_ps(_mm256_cmp_ps(r2, pulp::cast(zero), _CMP_EQ_OS),_mm256_rsqrt_ps(r2))
+        _mm256_andnot_ps(
+            _mm256_cmp_ps(r2, pulp::cast(zero), _CMP_EQ_OS),
+            _mm256_rsqrt_ps(r2),
+        )
     }
 }
 
 /// Single precision rsqrt
 pub fn rqsqrt_approx_intrin_sse_32(r2: __m128) -> __m128 {
     let zero = f32x4(0., 0., 0., 0.);
-    unsafe {
-        _mm_andnot_ps(_mm_cmpeq_ps(r2, pulp::cast(zero)),_mm_rsqrt_ps(r2))
-    }
+    unsafe { _mm_andnot_ps(_mm_cmpeq_ps(r2, pulp::cast(zero)), _mm_rsqrt_ps(r2)) }
 }
 
 /// Always apply single precision rsqrt instruction
 pub fn rqsqrt_approx_intrin_avx_64(r2: __m256d) -> __m256d {
-    unsafe {
-        _mm256_cvtps_pd(rqsqrt_approx_intrin_sse_32(_mm256_cvtpd_ps(r2)))
-    }
+    unsafe { _mm256_cvtps_pd(rqsqrt_approx_intrin_sse_32(_mm256_cvtpd_ps(r2))) }
 }
 
 pub fn rsqrt_newton_intrin_32(rinv: __m256, r2: __m256, newton_constant: f32) -> __m256 {
@@ -30,7 +33,13 @@ pub fn rsqrt_newton_intrin_32(rinv: __m256, r2: __m256, newton_constant: f32) ->
     // defer scaling 0.5
     unsafe {
         let newton_constant = _mm256_set1_ps(newton_constant);
-       _mm256_mul_ps(rinv, _mm256_sub_ps(newton_constant, _mm256_mul_ps(r2, _mm256_mul_ps(rinv, rinv))))
+        _mm256_mul_ps(
+            rinv,
+            _mm256_sub_ps(
+                newton_constant,
+                _mm256_mul_ps(r2, _mm256_mul_ps(rinv, rinv)),
+            ),
+        )
     }
 }
 
@@ -39,13 +48,17 @@ pub fn rsqrt_newton_intrin_64(rinv: __m256d, r2: __m256d, newton_constant: __m25
     // defer scaling by 0.5
 
     unsafe {
-
-       _mm256_mul_pd(rinv,  _mm256_sub_pd(newton_constant, _mm256_mul_pd(r2, _mm256_mul_pd(rinv, rinv))))
+        _mm256_mul_pd(
+            rinv,
+            _mm256_sub_pd(
+                newton_constant,
+                _mm256_mul_pd(r2, _mm256_mul_pd(rinv, rinv)),
+            ),
+        )
     }
 }
 
 pub fn rsqrt_single_intrin_32(r2: __m256) -> __m256 {
-
     // Approximate inverse
     let rinv = rqsqrt_approx_intrin_avx_32(r2);
 
@@ -55,9 +68,7 @@ pub fn rsqrt_single_intrin_32(r2: __m256) -> __m256 {
     rinv
 }
 
-
 pub fn rsqrt_single_intrin_64(r2: __m256d) -> __m256d {
-
     // Approximate inverse
     let rinv = rqsqrt_approx_intrin_avx_64(r2);
 
@@ -70,7 +81,6 @@ pub fn rsqrt_single_intrin_64(r2: __m256d) -> __m256d {
 }
 
 pub fn rsqrt_double_intrin_64(r2: __m256d) -> __m256d {
-
     // Approximate inverse
     let rinv = rqsqrt_approx_intrin_avx_64(r2);
 
